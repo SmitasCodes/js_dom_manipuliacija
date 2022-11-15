c6624b71c1b1b497fee7f447e671bc6847f47fd3
Moksleivis <Moksleivis@kitm.local>
2022-11-15 12:22:45 +0200

    Pridedamas dalinai sukurtas CSS ir JavaScript kintamieji su kuriais dirbsime

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const subject = document.querySelector('#subject').value;
+const priority = document.querySelector('#priority').value;
+const due = document.querySelector('#due').value;
+const status = document.querySelector('#status').value;
+const percent = document.querySelector('#percent').value;
