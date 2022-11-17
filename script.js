3db9360ae0293942698539d0bfcd87e4714c05de
Moksleivis <Moksleivis@kitm.local>
2022-11-17 14:17:03 +0200

    Progreso irasymas

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// Global kintamieji, skirti gauti input reiksmes
-// const subject = document.querySelector('#subject').value;
-// const priority = document.querySelector('#priority').value;
-// const due = document.querySelector('#due').value;
-// const status = document.querySelector('#status').value;
-// const percent = document.querySelector('#percent').value;
-
-
 function pagrindine() {
     let spausdinimoFunkcija = false;
     // Funkcija kuri sustabdo forma nuo atsinaujimo paspaudus submit
@@ -23,30 +15,83 @@ function pagrindine() {
     const due = document.querySelector('#due');
     const status = document.querySelector('#status');
     const percent = document.querySelector('#percent');
-    // Input objektas
-    arr = [{ subject: subject.value },
-    { priority: priority.value },
-    { due: due.value },
-    { status: status.value },
-    { percent: percent.value }];
+    // gaunama data
+    let today = new Date();
+    let dateTime = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+today.getHours()+"-"+today.getMinutes()+"-"+today.getSeconds();;
+    
+    
+    // Objektas atspaudinimui
+    arr =   [{ p: "p" },
+            { subject: subject.value },
+            { priority: priority.value },
+            { due: due.value },
+            { status: status.value },
+            { percent: percent.value},
+            { modified: dateTime }];
+
     // Ciklas kuris tikrina ar forma uzpildyta
     for (let key in arr) {
         for (let key2 in arr[key]) {
             if (arr[key][key2].length == 0) {
                 alert('Prasome uzpildyti forma pilnai');
                 return;
-            } else if (spausdinimoFunkcija != true) {
-                spausdinimas();
-                spausdinimoFunkcija = true;
+            } else(
+                spausdinimas(key2,arr[key][key2])
+            )
+        }
+    }
+    // Funkcija kuri atspausdins duomenis
+    function spausdinimas(raktas,verte){
+        tbody.appendChild(tr);
+        tr.classList.add('row')
+        let td = document.createElement('td');
+        td.classList.add(raktas + '-table');
+        td.innerHTML = verte;
+        if(raktas=="percent"){
+            let container = document.createElement('div');
+            container.classList.add("container");
+            let bar = document.createElement('div');
+            bar.classList.add('percent-bar');
+            container.appendChild(bar);
+            td.appendChild(container);
+            console.log(verte)
+            switch (verte){
+                case 0:
+                    bar.style.cssText = "width: 0;"
+                    break;
+                case 25:
+                    bar.style.cssText = "width: 25%;"
+                    break;
+                case 50:
+                    bar.style.cssText = "width: 50%;"
+                    break;
+                case 75:
+                    bar.style.cssText = "width: 75%;"
+                    break;
+                case 100:
+                    bar.style.cssText = "width: 100%;"
+                    break;
             }
         }
+        tr.appendChild(td);
+    }
 
-    }
-    // Funkcija kuri atspausdins table eilute su reikiamais elementais
-    function spausdinimas() {
-        tbody.appendChild(tr);
-        console.log(key2)
-    }
+    
 }
 
+
+
+
+
+// document.body.appendChild(container)
+
+
+
+// let container = document.createElement('div');
+//             container.classList.add("container");
+//             let bar = document.createElement('div');
+//             bar.classList.add('percent-bar');
+//             container.appendChild(bar)
+//             td.innerHTML = verte;
+//             tr.appendChild(td);
 
